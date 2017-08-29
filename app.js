9895a7d5646d1802062bdb5351c17f247126a3a1
christopherwperry <christopherwperry@gmail.com>
2017-08-29 16:46:45 -0400

    page renders addition of new books

--- app.js
+++ app.js
@@ -10,56 +10,58 @@ const expressValidator = require('express-validator');
 mongoose.connect('mongodb://localhost:27017/newdb', {useMongoClient: true});
 mongoose.Promise = require('bluebird');
 
+const DUPLICATE_RECORD_ERROR = 11000;
+
 app.use(express.static(__dirname + '/public'));
 app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
 
+app.get('/new', function(req, res){
+  res.render('new');
+})
+
+app.post('/new', function(req, res){
+  Book.create(req.body).then(function(book){
+     res.redirect('/');
+   })
+})
+
 app.get('/', function(req, res){
-  Book.find({}).then(function (books){
+  Book.find().then(function (books){
     res.render('index', {books: books})
   })
 })
 
-app.get('/new/', function(req, res){
-  res.render('new')
-})
-
-app.get('/:title', function(req, res){
-  Book.findOne({title: req.params.title}).then(function(book){
-    res.render('book', {books: books})
-  })
-})
-
-app.get('/:title/new_edition', function(req, res){
-  Book.findOne({title: req.params.title})
-  .then(function(book){
-    book.edition.push(req.body)
-    return book.save()
-    res.render('new_edition', {book: book})
-  })
-})
-
-app.post('/new', function(req, res){
-  Book.create(req.body).then(function (book){
-    res.redirect('/');
-  })
-  .catch(function (error){
-  //   let errorMsg
-  //   if (error.code === possible error){
-  //   enter error message
-  //   errorMsg = `The recipe name "${req.body.name}" has already been used`
-  // } else {
-  //   enter error message
-  //   errorMsg = "You have encountered an error!"
-  // }
-    res.send(error)
-  })
-})
-
 app.listen(port, function(){
-  console.log("The server is running on port 3000")
+ console.log("The server is running on port 3000")
 });
+
+// app.get('/:title/new_edition', function(req, res){
+//   Book.findOne({title: req.params.title})
+//   .then(function(book){
+//     book.edition.push(req.body);
+//     book.save();
+//     res.render('new_edition', {book: book})
+//   })
+// })
+
+  //  .catch(function (error) {
+  //    let errorMsg;
+  //    if (error.code ===
+  //      DUPLICATE_RECORD_ERROR) {
+  //        errorMsg = `The Title "${req.body.title}" has already been used.`
+  //      } else {
+  //        errorMsg = "You have encountered an unknown error."
+  //   }
+  //   res.render('new', {errorMsg: errorMsg});
+  // })
+
+// app.get('/:title', function(req, res){
+//     Book.findOne({title: req.params.title}).then(function(book){
+//       res.render('book', {book: book})
+//     })
+//   })
--- models/books.js
+++ models/books.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-    title: { type: String, required: true, unique: true },
+    title: { type: String, required: true},
     author: {type: String, required: true},
     genre: {type: String},
-    pages: Number,
-    year: Number,
-    editions: [{
-        year: { type: Number, required: true},
-        publisher: { type: String},
-        location: { type: String, required: true }
-    }]
+    // pages: Number,
+    // year: Number,
+    // isbn: Number,
+    // language: {type: String},
+    // editions: [{
+    //     year: { type: Number, required: true},
+    //     publisher: { type: String},
+    //     location: { type: String}
+    // }]
 })
 
 const Book = mongoose.model('Book', bookSchema);
-
-var book = new Book();
-
-book.save();
 
 module.exports = Book;
