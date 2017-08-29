aba7455b862a3e8144750c8c613eca6fd74e234b
christopherwperry <christopherwperry@gmail.com>
2017-08-29 14:06:03 -0400

    add content to views and establish connection to database

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const mustache = require('mustache-express');
 const Book = require('./models/books')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 mongoose.connect('mongodb://localhost:27017/newdb', {useMongoClient: true});
 mongoose.Promise = require('bluebird');
 
@@ -12,22 +14,50 @@ app.use(express.static(__dirname + '/public'));
 app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 app.get('/', function(req, res){
-//  const books = db.collection('books');
   Book.find({}).then(function (books){
-    res.render('index', {books})
+    res.render('index', {books: books})
   })
 })
 
-app.get('/new', function(req, res){
+app.get('/new/', function(req, res){
   res.render('new')
 })
 
+app.get('/:title', function(req, res){
+  Book.findOne({title: req.params.title}).then(function(book){
+    res.render('book', {books: books})
+  })
+})
+
+app.get('/:title/new_edition', function(req, res){
+  Book.findOne({title: req.params.title})
+  .then(function(book){
+    book.edition.push(req.body)
+    return book.save()
+    res.render('new_edition', {book: book})
+  })
+})
+
 app.post('/new', function(req, res){
-  const books = db.collection('books');
-  books.insert({"title": req.body.title, "author": req.body.author, "genre": req.body.genre, "pages": req.body.pages, "year": req.body.year});
-  res.redirect('/');
+  Book.create(req.body).then(function (book){
+    res.redirect('/');
+  })
+  .catch(function (error){
+  //   let errorMsg
+  //   if (error.code === possible error){
+  //   enter error message
+  //   errorMsg = `The recipe name "${req.body.name}" has already been used`
+  // } else {
+  //   enter error message
+  //   errorMsg = "You have encountered an error!"
+  // }
+    res.send(error)
+  })
 })
 
 app.listen(port, function(){
